9356a42341f82d893502fe285338dcde677a3e3f
qw <qwzhang01@qq.com>
2026-03-11 16:57:33 +0800

    fix bugs

--- web_site/src/views/trading/TradingPage.vue
+++ web_site/src/views/trading/TradingPage.vue
@@ -16,9 +16,7 @@
                   v-for="symbol in availableSymbols" 
                   :key="symbol.value"
                   :value="symbol.value"
-                >
-                  {{ symbol.value }}
-                </option>
+                >{{ symbol.value }}</option>
               </select>
               <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-dark-100">
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
